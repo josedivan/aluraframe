2db9c8d138597625e33b1504e98c9705066965d6
Edivan <edivanlima23@gmail.com>
2021-03-27 23:28:41 -0300

    juste mês

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,6 +1,20 @@
 class NegociacaoController {
+  constructor() {
+    let $ = document.querySelector.bind(document);
+
+    this._inputData = $("#data");
+    this._inputQuantidade = $("#quantidade");
+    this._inputValor = $("#valor");
+  }
+
   adiciona(event) {
     event.preventDefault();
-    alert("Chamei ação no controller");
+
+    let data = new Date(
+      ...this._inputData.value.split("-").map(function (item, indice) {
+        return item - (indice % 2);
+      }),
+    );
+    console.log(data);
   }
 }
